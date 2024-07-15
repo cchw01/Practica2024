cc3d5e322cb46e81aefbd45b6b2371e935f009e8
alexandradume <alexandra.teodora.dume@stud.ubbcluj.ro>
2024-07-15 10:45:11 +0300

    Added AircraftItem

---
+++ Frontend/src/app/app-logic/aircraft-item.ts
@@ -0,0 +1,10 @@
+export class AircraftItem{
+    RegistrationNumber!: number;
+    Maker!:string;
+    Model!:string;
+    NumberOfSeats!:number;
+    AutonomyInHours!:number;
+    MaxCargo!:number;
+
+
+}
