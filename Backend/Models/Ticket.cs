666151c005e6ef88f79b686de0d2af1d69d20785
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-15 10:39:11 +0300

    Added Tickets Model

---
+++ Backend/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public virtual Flight Flight { get; set; }
+        public virtual User Passenger { get; set; }
+        public virtual CheckIn CheckIn { get; set; }
+        public bool Luggage { get; set; }
+        public float Price { get; set; }
+
+    }
+}
