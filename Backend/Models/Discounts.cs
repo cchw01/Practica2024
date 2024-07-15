177f339b1813349cae7beb01c7401a3d5b504ab7
Sunless19 <onofreisergiu70@gmail.com>
2024-07-15 11:31:56 +0300

    Added discounts model

---
+++ Backend/Models/Discounts.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class Discounts
+    {
+        public int DiscountId { get; set; }
+        public string DiscountName { get; set; }
+        public string DiscountDescription { get; set;}
+        public Flight Flight { get; set; }
+        public int DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
