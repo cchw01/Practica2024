f5e9cf8a86146fb8861118e46a049d02c470fb5e
Dorin <dorinpetrutuscoiu@gmail.com>
2024-07-15 11:30:17 +0300

    Added AiportModel

---
+++ Backend/Models/Airport.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class Airport
+    {
+        public int AirportId { get; set; }
+        public string Location { get; set; }
+
+        public string AirportName { get; set; }
+
+    }
+}
