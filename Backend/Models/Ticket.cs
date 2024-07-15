1176ad8f730239c0256d53d24f66ded22eec7cd0
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-15 11:05:36 +0300

    Added Ticket Model

---
+++ Backend/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Ticket
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int TicketId { get; set; }
+        public virtual Flight Flight { get; set; }
+        public virtual User Passenger { get; set; }
+        public virtual CheckIn CheckIn { get; set; }
+        public bool Luggage { get; set; }
+        public float Price { get; set; }
+
+    }
+}
