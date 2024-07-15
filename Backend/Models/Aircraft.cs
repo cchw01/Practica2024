74b81153f4846c40e40e9b5695e9a8e94b7f0db2
Sunless19 <onofreisergiu70@gmail.com>
2024-07-15 10:41:46 +0300

    Update Aircraft.cs

--- Backend/Models/Aircraft.cs
+++ Backend/Models/Aircraft.cs
@@ -1,6 +1,17 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
 {
     public class Aircraft
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        private string RegistrationNumber { get; set; }
+        private string Maker { get; set; }
+        private string Model { get; set; }
+        private int NumberOfSeats { get; set; }
+        private int AutonomyInHours { get; set; }
+        private double MaxCargo { get; set; }
     }
 }
