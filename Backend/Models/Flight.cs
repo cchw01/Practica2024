0284edb27e2e795da42e4113f9ba5a4745f17aaf
Ghita Valentin-Gabriel <119352194+Valy2255@users.noreply.github.com>
2024-07-15 12:42:07 +0300

    updated flight model

--- Backend/Models/Flight.cs
+++ Backend/Models/Flight.cs
@@ -2,16 +2,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
+using Backend.Models;
 
-public class TheFlights
+public class Flight
 
 {
 
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
     public int FlightNumber { get; set; }
-    public virtual Airoport DepartingAeroport { get; set; }
-    public virtual Airoport DestinationAeroport { get; set; }
+    public virtual Airport DepartingAeroport { get; set; }
+    public virtual Airport DestinationAeroport { get; set; }
     public DateTime DepartingTime { get; set; }
     public DateTime FlightTime { get; set; }
     public int FlightCost { get; set; }
