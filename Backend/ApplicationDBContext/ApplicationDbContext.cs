652aebdc6529be238cebddd3ec8116c3ae0146d1
Sunless19 <onofreisergiu70@gmail.com>
2024-07-15 17:03:06 +0300

    Context for airport and aircraft.

---
+++ Backend/ApplicationDBContext/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.ApplicationDBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Aircraft> Aircrafts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PlaneCompanyDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Backend/Migrations/20240715135937_initial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aircrafts",
+                columns: table => new
+                {
+                    RegistrationNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Maker = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfSeats = table.Column<int>(type: "int", nullable: false),
+                    AutonomyInHours = table.Column<int>(type: "int", nullable: false),
+                    MaxCargo = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aircrafts", x => x.RegistrationNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    AirportId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AirportName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.AirportId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aircrafts");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
