68ba551e947bcef34f9de7c4048ba558609f1171
Ghita Valentin-Gabriel <119352194+Valy2255@users.noreply.github.com>
2024-07-15 11:23:08 +0300

    Added CheckIn Model

---
+++ Backend/Models/CheckIn.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend.Models
+{
+    public enum IdDocumentType
+    {
+        IdentityCard,
+        Passport,
+        DriverLicense
+    }
+    public class CheckIn
+    {
+
+
+        public int CheckInId { get; set; }
+        public Ticket Ticket { get; set; }
+        public string PassengerName { get; set; }
+        public IdDocumentType IdDocumentType { get; set; }
+        public string DocumentData { get; set; }
+        public bool CheckInStatus { get; set; }
+        public string PassengerEmail { get; set; }
+
+    }
+}
