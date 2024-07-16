a24b78f0021607697901f2341d3fabae89c81b71
DorinUscoiu1 <uscoiudori7@gmail.com>
2024-07-16 11:34:19 +0300

    CRUD Airport

---
+++ Backend/Services/AirportManager.cs
@@ -0,0 +1,51 @@
+﻿using Backend.Models;
+using System.Xml.Serialization;
+using Backend.ApplicationDBContext;
+namespace Backend.Services
+{
+    public class AirportManager
+    {
+        private readonly ApplicationDbContext airportContext;
+        public AirportManager()
+        {
+            airportContext = new ApplicationDbContext();
+        }
+        public List<Airport> GetAirports()
+        {
+            return airportContext.Airports.ToList();
+        }
+        public Airport GetAirport(int id)
+        {
+            return airportContext.Airports.FirstOrDefault(x => x.AirportId == id);
+        }
+        public void AddAirport(Airport airport)
+        {
+            airportContext.Airports.Add(airport);
+            airportContext.SaveChanges();
+        }
+        public void RemoveAirport(int id)
+        {
+            var airport = airportContext.Airports.FirstOrDefault(x => x.AirportId == id);
+            if (airport == null)
+            {
+                throw new ArgumentException("Airport does not exist");
+            }
+            airportContext.Airports.Remove(airport);
+            airportContext.SaveChanges();
+        }
+        public void UpdateAirport(Airport airport)
+        {
+            var oldAirport = airportContext.Airports.FirstOrDefault(x => x.AirportId == airport.AirportId);
+            if (oldAirport == null)
+            {
+                throw new ArgumentException("Airport does not exist");
+            }
+            oldAirport.AirportName = airport.AirportName;
+            oldAirport.Location = airport.Location;
+
+            airportContext.Airports.Update(oldAirport);
+            airportContext.SaveChanges();
+        }
+
+    }
+}
