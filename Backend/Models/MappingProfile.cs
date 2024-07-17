68b289ac5836046c8681a3ff612dc717fe7aa18d
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-17 18:57:55 +0300

    Finished MappingProfile

--- Backend/Models/MappingProfile.cs
+++ Backend/Models/MappingProfile.cs
@@ -9,7 +9,32 @@ namespace Backend.Models
         {
             CreateMap<Aircraft, AircraftDto>().ReverseMap();
             CreateMap<AircraftDto, Aircraft>()
-                .ForMember(dest => dest.AircraftId, opt => opt.Ignore()); 
+                .ForMember(dest => dest.AircraftId, opt => opt.Ignore());
+
+            CreateMap<Airport, AirportDto>().ReverseMap();
+            CreateMap<AirportDto, Airport>()
+                .ForMember(dest => dest.AirportId, opt => opt.Ignore());
+
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<UserDto, User>()
+                .ForMember(dest => dest.UserId, opt => opt.Ignore());
+                
+            CreateMap<Flight, FlightDto>().ReverseMap();
+            CreateMap<FlightDto, Flight>()
+                .ForMember(dest => dest.FlightNumber, opt => opt.Ignore());
+
+            CreateMap<Ticket, TicketDto>().ReverseMap();
+            CreateMap<TicketDto, Ticket>()
+                .ForMember(dest => dest.TicketId, opt => opt.Ignore());
+
+            CreateMap<Discount, DiscountDto>().ReverseMap();
+            CreateMap<DiscountDto, Discount>()
+                .ForMember(dest => dest.DiscountId, opt => opt.Ignore());
+
+            CreateMap<CheckIn, CheckInDto>().ReverseMap();
+            CreateMap<CheckInDto, CheckIn>()
+                .ForMember(dest => dest.CheckInId, opt => opt.Ignore());
         }
+
     }
 }
