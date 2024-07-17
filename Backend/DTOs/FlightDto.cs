5093bc72a4b7c0b9a1f939af5b24514ef53aa6bd
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-17 16:44:49 +0300

    Added DTOs

---
+++ Backend/DTOs/FlightDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.DTOs
+{
+    public class FlightDto
+    {
+        public int FlightNumber { get; set; }
+        public int DepartingAirportId { get; set; }
+        public int DestinationAirportId { get; set; }
+        public int AircraftId { get; set; }
+        public DateTime DepartingTime { get; set; }
+        public DateTime FlightTime { get; set; }
+        public int FlightCost { get; set; }
+        public int? DiscountId { get; set; }
+    }
+
+}
---
+++ Backend/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string EmailAddress { get; set; }
+    }
+
+}
