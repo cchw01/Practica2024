1a65439944b69ca99ef732807fe29220569b7836
beVoli <vchiriac68@yahoo.com>
2024-07-18 11:28:21 +0300

    update

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,31 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './menu-items/home-page/home-page.component';
-import { ContactComponent } from './menu-items/contact/contact.component';
-import { LoginComponent } from './menu-items/login/login.component';
 import { CheckInComponent } from './menu-items/check-in/check-in.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlightsComponent } from './menu-items/flights/flights.component';
+import { ContactComponent } from './menu-items/contact/contact.component';
 import { FlightComponent } from './menu-items/flights/flight/flight.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BookingComponent } from './menu-items/booking/booking.component';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { TicketsComponent } from './menu-items/booking/tickets/tickets.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { UserProfileComponent } from './menu-items/user-profile/user-profile.component';
-import { RegisterComponent } from './menu-items/register/register.component';
+import { FlightsComponent } from './menu-items/flights/flights.component';
+import { HomePageComponent } from './menu-items/home-page/home-page.component';
+import { LoginComponent } from './menu-items/login/login.component';
+import { ArrivalTimePipe } from './app-logic/pipes/arrivalTime.pipe';
 import { DiscountPipe } from './app-logic/pipes/discountPrice.pipe';
 import { FormatFlightTime } from './app-logic/pipes/formatFlightTime.pipe';
-import { ArrivalTimePipe } from './app-logic/pipes/arrivalTime.pipe';
+import { BookingComponent } from './menu-items/booking/booking.component';
+import { TicketsComponent } from './menu-items/booking/tickets/tickets.component';
+import { RegisterComponent } from './menu-items/register/register.component';
+import { UserProfileComponent } from './menu-items/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
