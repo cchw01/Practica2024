7f64c36e69982deb9fcd71ce2022177fc3d52772
Sunless19 <onofreisergiu70@gmail.com>
2024-07-19 12:37:00 +0300

    Update airport-service.ts

--- Frontend/src/app/app-logic/services/airport-service.ts
+++ Frontend/src/app/app-logic/services/airport-service.ts
@@ -10,33 +10,7 @@ export class AirportListMockService {
 
   apiUrl="http://localhost:5198/api/Airport";
 
-  airportsData: Array<AirportItem> = [
-    {
-      airportId: 0    ,
-      airportName: '',
-      location: '',
-    },
-    {
-      airportId: 1,
-      airportName: 'AeroportA',
-      location: 'LocatiaA',
-    },
-    {
-      airportId: 2,
-      airportName: 'AeroportB',
-      location: 'LocatiaB',
-    },
-    {
-      airportId: 3,
-      airportName: 'AeroportC',
-      location: 'LocatiaC',
-    },
-    {
-      airportId: 4,
-      airportName: 'AeroportD',
-      location: 'LocatiaD',
-    },
-  ];
+  
   constructor(private httpclient:HttpClient) {}
   getDataAirports():Observable<Array<AirportItem>> {
     return this.httpclient.get<Array<AirportItem>>(this.apiUrl);
@@ -59,14 +33,7 @@ export class AirportListMockService {
     });
   }
 
-  getLastId(): number {
-    return Math.max.apply(
-      Math,
-      this.airportsData.map(function (o) {
-        return o.airportId;
-      })
-    );
-  }
+ 
   deleteItem(id:number):Observable<AirportItem>{
     return this.httpclient.delete<AirportItem>(this.apiUrl+"/"+id);
   }
