8fb5fc4eb365d655bde12e4e161df45b038c3623
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-19 16:35:54 +0300

    Modified TicketItem and Added all required methods in UserService

--- Frontend/src/app/app-logic/services/user.service.ts
+++ Frontend/src/app/app-logic/services/user.service.ts
@@ -1,68 +1,98 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { UserDto } from '../DTOs/user-dto';
 import { UserItem } from '../models/user-item';
+import { TicketItem } from '../models/ticket-item';
 
 import * as CryptoJS from 'crypto-js';
+import { LoginDto } from '../DTOs/login-dto';
+import { FlightItem } from '../models/flight-item';
+import { TicketDto } from '../DTOs/ticket-dto';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UserService {
-  apiUrl = 'https://localhost:5198/api/Airport';
-
-  userData: Array<UserItem> = [];
+  private apiUrl = 'http://localhost:5198/api/User';
 
   constructor(private httpClient: HttpClient) {}
 
-  getUsers(): Observable<Array<UserItem>> {
-    return this.httpClient.get<Array<UserItem>>(this.apiUrl);
-  }
-
-  addUser(user: UserItem): void {
-    var result;
-    this.httpClient.post<UserItem>(this.apiUrl, user).subscribe((data) => {
-      result = data;
-      console.log(result);
-    });
-  }
-
-  updateUser(user: UserItem): void {
-    var result;
-    this.httpClient.put<UserItem>(this.apiUrl, user).subscribe((data) => {
-      result = data;
-      console.log(result);
-    });
-  }
-
-  deleteUser(id: number): void {
-    const options = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-      body: { id: id },
-    };
-    this.httpClient.delete<void>(this.apiUrl, options).subscribe({
-      next: (data) => {
-        console.log('Delete successful', data);
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      },
-    });
-  }
-  getLastId(): number {
-    return Math.max.apply(
-      Math,
-      this.userData.map(function (o) {
-        return o.userId;
-      })
+  getUsers(): Observable<UserItem[]> {
+    return this.httpClient.get<UserDto[]>(this.apiUrl).pipe(
+      map(usersDto => usersDto.map(dto => new UserItem(dto)))
     );
   }
 
   getUserById(id: number): Observable<UserItem> {
-    return this.httpClient.get<UserItem>(this.apiUrl + '/' + id);
-    //return this.inventoryData.filter((x) => x.id == id)[0];
+    return this.httpClient.get<UserDto>(`${this.apiUrl}/${id}`).pipe(
+      map(dto => new UserItem(dto))
+    );
+  }
+
+  addUser(user: UserItem) {
+    const hashedPassword = this.hashPassword(user.password);
+    const userDto = {
+      userId: 0,
+      name: user.name,
+      role: user.role,
+      emailAddress: user.emailAddress,
+      password: hashedPassword
+    };
+    return this.httpClient.post<UserDto>(this.apiUrl, userDto);
+  }
+
+  updateUser(user: UserItem): Observable<UserItem> {
+    const hashedPassword = this.hashPassword(user.password);
+    const userDto = {
+      userId: 0,
+      name: user.name,
+      role: user.role,
+      emailAddress: user.emailAddress,
+      password: hashedPassword
+    };
+    return this.httpClient.put<UserDto>(`${this.apiUrl}/${user.userId}`, userDto);
+  }
+
+  deleteUser(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getUserTickets(userId: number): Observable<TicketItem[]> {
+    return this.httpClient.get<TicketDto[]>(`${this.apiUrl}/${userId}/tickets`).pipe(
+      map(ticketsDto => ticketsDto.map(ticketDto => new TicketItem({
+        tickedId: ticketDto.ticketId,
+        flightId: ticketDto.flightId,
+        userId: ticketDto.userId,
+        checkInId: ticketDto.checkInId,
+        luggage: ticketDto.luggage,
+        price: ticketDto.price,
+        flight: new FlightItem(),  
+        passager: new UserItem(), 
+        checkIn: ticketDto.checkInId !== undefined  
+      })))
+    );
+  }
+
+  register(user: UserItem) {
+    const hashedPassword = this.hashPassword(user.password);
+    const userDto = {
+      userId: 0,
+      name: user.name,
+      role: user.role,
+      emailAddress: user.emailAddress,
+      password: hashedPassword
+    };
+    return this.httpClient.post<UserDto>(`${this.apiUrl}/register`, userDto);
+}
+
+  login(email: string, password: string): Observable<UserItem> {
+    const hashedPassword = this.hashPassword(password);
+    const loginDto : LoginDto = { emailAddress: email, password: hashedPassword };
+    return this.httpClient.post<UserDto>(`${this.apiUrl}/login`, loginDto).pipe(
+      map(dto => new UserItem(dto))
+    );
   }
 
   hashPassword(password: string): string {
