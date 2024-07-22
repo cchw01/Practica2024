54a2729de367e117fb31b5fb2635faae037d1a21
DorinUscoiu1 <uscoiudori7@gmail.com>
2024-07-22 11:46:57 +0300

    New Ticket endpoint

--- Backend/Controllers/TicketController.cs
+++ Backend/Controllers/TicketController.cs
@@ -84,7 +84,13 @@ namespace Backend.Controllers
 			}
 
 		}
+        [HttpGet("search")]
+        public IActionResult SearchTickets([FromQuery] int departingAirportId, [FromQuery] int destinationAirportId, [FromQuery] DateTime departureDate, [FromQuery] DateTime returnDate)
+        {
+            var tickets = ticketManager.GetTicketsBySearchCriteria(departingAirportId, destinationAirportId, departureDate, returnDate);
+            return Ok(tickets);
+        }
 
 
-	}
+    }
 }
