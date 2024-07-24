89d589f80b578f2c687547bb9350d1c72bd32e55
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-07-24 16:24:49 +0300

    Added validation if password is null or empty when updating a user

--- Backend/DTOs/UserDto.cs
+++ Backend/DTOs/UserDto.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Role { get; set; }
         public string EmailAddress { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 
 }
