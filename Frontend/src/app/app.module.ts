9f8eaeea5e1095fca5924f13dabc7573e5366b64
David-UniTBv <secareanu.david2003@gmail.com>
2024-07-24 16:25:14 +0300

    test-commit

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -54,7 +54,12 @@ import { AddCheckInComponent } from './menu-items/admin/check-in-admin/add-check
 import { QrCodeModule } from 'ng-qrcode';
 import { format, parse } from 'date-fns';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
+import { CarouselModule } from 'primeng/carousel';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -111,6 +116,11 @@ import { format, parse } from 'date-fns';
     MatNativeDateModule,
     MatDatepickerModule,
     QrCodeModule,
+    MatCardModule,
+    MatListModule,
+    CarouselModule,
+    CardModule,
+    ButtonModule,
   ],
   providers: [provideAnimationsAsync(), provideHttpClient()],
   bootstrap: [AppComponent],
